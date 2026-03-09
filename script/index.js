88ce05dfafb575d88ab0e6e060d5080944d364ee
nabilanur1 <nabila.jess@gmail.com>
2026-03-09 15:07:42 +0600

    added load issues all,open and closed in index.js files

--- script/index.js
+++ script/index.js
@@ -11,5 +11,41 @@ function login() {
         alert("Invalid credentials! Try: admin / admin123");
     }
 }
+// 2. Load Issues (All/Open/Closed)
+let allIssuesData = [];
+
+async function loadIssues(filter = 'all', element = null) {
+    const container = document.getElementById("issuesContainer");
+    const loader = document.getElementById("loader");
+    
+    
+    if(element) {
+        document.querySelectorAll('.tab-item').forEach(btn => {
+            btn.classList.remove('bg-[#5811f5]', 'text-white');
+        });
+        element.classList.add('bg-[#5811f5]', 'text-white');
+    }
+
+    loader.classList.remove('hidden');
+    container.innerHTML = "";
+
+    try {
+        const response = await fetch(`${API_URL}/issues`);
+        const result = await response.json();
+        allIssuesData = result.data;
+
+        let filteredData = allIssuesData;
+        if (filter !== 'all') {
+            filteredData = allIssuesData.filter(issue => issue.status === filter);
+        }
+
+        document.getElementById("issueCount").innerText = filteredData.length;
+        displayCards(filteredData);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    } finally {
+        loader.classList.add('hidden');
+    }
+}
 
 
